1aaac6e50b2c894ca76e99e9e195ab5ec277c895
jDudek <90628819+DudekJakub@users.noreply.github.com>
2022-04-23 21:11:15 +0200

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ $(document).ready(function() {
    getAllTasks();
 
    function getAllAvailableBoards(callback, callbackArgs) {
-      var requestUrl = trelloApiRoot + 'getTrelloBoardse';
+      var requestUrl = trelloApiRoot + 'getTrelloBoards';
 
       $.ajax({
          url: requestUrl,
